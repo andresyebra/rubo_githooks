87c34e760f3dc225aba23da15b870479344d3c52
Andres Yebra <andres.yebra@bigcommerce.com>
2022-10-14 10:59:45 -0700

    PR-0: Rubocop siggestions. (#2)
    * PR-0: Rubocop suggestions

--- lib/user.rb
+++ lib/user.rb
@@ -5,20 +5,16 @@ class User
   attr_reader :name, :age
 
   def initialize(name:, age:)
-    @name = name
-    @age  = age
+    @name, @age = name, age
   end
 
   # @param other [User]
   # @return boolean
   def ==(other)
     return super unless other.is_a?(User)
-
     @name == other.name && @age == other.age
   end
 
   # @return [String]
-  def inspect
-    "<#{self.class} name=#{name.inspect}, age=#{age.inspect}>"
-  end
+  def inspect = "<#{self.class} name=#{name.inspect}, age=#{age.inspect}>"
 end
--- main.rb
+++ main.rb
@@ -1,10 +1,5 @@
-# frozen_string_literal: true
 
 require './lib/user'
 
-def main
-  user = User.new(name: 'John', age: 20)
-  user.age
-end
-UserNew = Struct.new(:name, :age)
-main
+user = User.new(name: 'John', age: 20)
+puts user.inspect
